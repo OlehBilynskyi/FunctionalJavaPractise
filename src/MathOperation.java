dff70d3d709853a43b3d7895c32a0400278ace3e
Oleh Bilynskyi <obslinskij@gmail.com>
2025-05-29 00:06:31 +0300

    Add MathOperation and StringManipulator Interface

---
+++ src/MathOperation.java
@@ -0,0 +1,6 @@
+//Оголошуємо функціональний інтерфейс (має лише один абстрактний метод)
+@FunctionalInterface
+public interface MathOperation {
+    //Абстрактний метод operate приймає два цілих числа a і b, повертає ціле число
+    int operate(int a, int b);
+}
---
+++ src/StringManipulator.java
@@ -0,0 +1,6 @@
+@FunctionalInterface
+//Створюємо публічний інтерфейс для обробки рядків
+public interface StringManipulator {
+    //Абстрактний метод, який повертає рядок s і повертає результат обробки цього рядка
+    String manipulate(String s);
+}
